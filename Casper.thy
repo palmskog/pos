18a782309d470bb9ca35cc3e81be5442ab719d65
Giuliano Losa <giuliano@losa.fr>
2017-09-15 11:15:49 -0700

    Nicer formalization
    But slashing condition 2 deviates from Vitalik's description.

--- Casper.thy
+++ Casper.thy
@@ -17,21 +17,24 @@ record ('n,'h,'v)state =
 locale casper = byz_quorums +
   views:linorder view_less_eq view_less + 
   order_bot  bot view_less_eq view_less for
-  hash_less_eq :: "'h \<Rightarrow> 'h \<Rightarrow> bool" (infix "\<le>\<^sub>h" 50)
-  and hash_less :: "'h \<Rightarrow> 'h \<Rightarrow> bool" (infix "<\<^sub>h" 50)
-  and view_less_eq :: "'v \<Rightarrow> 'v \<Rightarrow> bool" (infix "\<le>\<^sub>v" 50)
+  view_less_eq :: "'v \<Rightarrow> 'v \<Rightarrow> bool" (infix "\<le>\<^sub>v" 50)
   and view_less :: "'v \<Rightarrow> 'v \<Rightarrow> bool" (infix "<\<^sub>v" 50)
-  and bot::"'v" ("\<bottom>")
+  and bot::"'v" ("\<bottom>") +
+fixes 
+  hash_pred :: "'h \<Rightarrow> 'h \<Rightarrow> bool" (infix "\<leftarrow>" 50)
+assumes
+  -- "a hash has at most one predecessor which is not itself"
+  "\<And> h1 h2 . h1 \<leftarrow> h2 \<Longrightarrow> h1 \<noteq> h2"
+  and "\<And> h1 h2 h3 . \<lbrakk>h2 \<leftarrow> h1; h3 \<leftarrow> h1\<rbrakk> \<Longrightarrow> h2 = h3"
 begin
+
+inductive hash_ancestor (infix "\<leftarrow>\<^sup>*" 50) where 
+  "h1 \<leftarrow> h2 \<Longrightarrow> h1 \<leftarrow>\<^sup>* h2"
+| "\<lbrakk>h1 \<leftarrow>\<^sup>* h2; h2 \<leftarrow>\<^sup>* h3\<rbrakk> \<Longrightarrow> h1 \<leftarrow>\<^sup>* h3"
 
 lemmas casper_assms_def = casper_def class.linorder_def class.order_def
     class.preorder_def class.order_axioms_def class.linorder_axioms_def byz_quorums_def
     class.order_bot_def class.order_bot_axioms_def
-
-definition is_pred where 
-  -- "predecessor"
-  "is_pred h1 h2 \<equiv> h1 <\<^sub>h h2 \<and> (\<forall> h . \<not> (h1 <\<^sub>h h \<and> h <\<^sub>h h2))" 
-notation is_pred (infix "\<leftarrow>" 50)
 
 text {* All messages in epoch @{term \<bottom>} are ignored;  @{term \<bottom>} is used as a special value. *}
 
@@ -41,11 +44,12 @@ definition committed where
   "committed s q h v \<equiv> v \<noteq> bot \<and> (\<forall> n . n \<in>\<^sub>1 q \<longrightarrow> commit_msg s n h v)"
 definition fork where 
   "fork s \<equiv> \<exists> h1 h2 q1 q2 v1 v2 . committed s q1 h1 v1 \<and> committed s q2 h2 v2
-    \<and> \<not>(h2 \<le>\<^sub>h h1 \<or> h1 \<le>\<^sub>h h2)"
+    \<and> \<not>(h2 \<leftarrow>\<^sup>* h1 \<or> h1 \<leftarrow>\<^sup>* h2 \<or> h1 = h2)"
 
 definition slashed_1 where  "slashed_1 s n \<equiv> 
   \<exists> h v . commit_msg s n h v \<and> (\<forall> q v2 . v2 <\<^sub>v v \<longrightarrow> \<not>prepared s q h v v2)"
 definition slashed_2 where
+  -- "this is not exactly what Vitalik wrote."
   "slashed_2 s n \<equiv>
   \<exists> h v1 v2 . prepare_msg s n h v1 v2 \<and> v2 \<noteq> \<bottom> \<and>
     (\<forall> v3 q h2 . v3 <\<^sub>v v2 \<longrightarrow> \<not>(h2 \<leftarrow> h \<and> prepared s q h2 v2 v3))"
@@ -57,32 +61,27 @@ definition slashed_4 where
   "slashed_4 s n \<equiv> \<exists> h1 h2 v v1 v2 . (h1 \<noteq> h2 \<or> v1 \<noteq> v2) \<and> 
     prepare_msg s n h1 v v1 \<and> prepare_msg s n h2 v v2"
 
-definition slashed_2a where "slashed_2a s n \<equiv>
-  \<exists> h v1 . prepare_msg s n h v1 \<bottom> \<and> 
-    (\<forall> h2 q v2 . h2 \<leftarrow> h \<longrightarrow> \<not>prepared s q h2 \<bottom> v2)"
-definition slashed_5 where 
-  "slashed_5 s n \<equiv> \<exists> h v1 v2 . prepare_msg s n h v1 v2 \<and> v1 \<noteq> \<bottom> \<and> v1 \<le>\<^sub>v v2"
-
 definition slashed where "slashed s n \<equiv> 
   slashed_1 s n \<or> slashed_2 s n \<or> slashed_3 s n \<or>  slashed_4 s n"
 
 lemma assumes "fork s"
   shows "\<exists> q . \<forall> n . n \<in>\<^sub>2 q \<longrightarrow> slashed s n"
   using [[]]
-  nitpick[verbose, card 'b=2, card 'c=2, card 'a=5, card 'v=6, card 'h=5, card 'd=1,
-      card "('a, 'h, 'v, 'd) state_scheme" = 1, dont_box] (*,
+  nitpick[verbose, card 'b=2, card 'c=2, card 'a=5, card 'v=8, card 'h=7, card 'd=1,
+      card "('a, 'h, 'v, 'd) state_scheme" = 1, dont_box, timeout=300] (*,
       eval="(slashed s, committed s)"] *)
   using assms casper_axioms
   apply -
   unfolding slashed_def slashed_1_def slashed_2_def slashed_4_def slashed_3_def
-      prepared_def is_pred_def fork_def committed_def slashed_2a_def
+      prepared_def fork_def committed_def slashed_2a_def
   apply (cases s)
   apply simp
   unfolding casper_assms_def
   apply (simp (no_asm_use))
   apply (match premises in P[thin]:"?x = ?y" \<Rightarrow> \<open>-\<close>)
   apply clarify
-  using [[smt_timeout=3000,smt_oracle=true,smt_random_seed=4542,smt_certificates="./casper.cert"]]
+  oops
+  using [[smt_timeout=3000,smt_oracle=true,smt_random_seed=456363655542,smt_certificates="./casper.cert"]]
   sledgehammer[timeout=5000, provers=z3 spass]()
 
 
