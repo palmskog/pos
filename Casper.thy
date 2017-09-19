b3b2182935629aae4facd86573fea23eb4f4008a
Giuliano Losa <giuliano@losa.fr>
2017-09-19 09:31:58 -0700

    cosmetic changes.

--- Casper.thy
+++ Casper.thy
@@ -1,52 +1,56 @@
 theory Casper
-imports Main "HOL-Eisbach.Eisbach" Inductive
+  imports Main
 begin
 
-locale byz_quorums =
-  fixes member_1 :: "'n \<Rightarrow> 'q1 \<Rightarrow> bool" (infix "\<in>\<^sub>1" 50)(* membership in 2/3 set *)
-    and member_2 :: "'n \<Rightarrow> 'q2 \<Rightarrow> bool" (infix "\<in>\<^sub>2" 50)(* membership in 1/3 set *)
-  assumes "\<And> q1 q2 . \<exists> q3 . \<forall> n . n \<in>\<^sub>2 q3 \<longrightarrow> n \<in>\<^sub>1 q1 \<and> n \<in>\<^sub>1 q2"  (* 2/3 quorums have 1/3 intersection *)
+text {* We use first-order modeling as much as possible. This allows to reduce the size of the model, 
+  and also the size of the proofs from more than 1000 lines in Yoichi's proof to less than a 100. *}
 
-record ('n,'h,'v)state =
-  -- "@{typ 'n} is the type of validators (nodes), @{typ 'h} hashes, and @{typ nat} views"
+locale byz_quorums =
+  -- "Here we fix two types @{typ 'q1} and @{typ 'q2} for quorums of cardinality greater than 2/3 of 
+the validators and quorum of cardinality greater than 1/3 of the validators."
+  fixes member_1 :: "'n \<Rightarrow> 'q1 \<Rightarrow> bool" (infix "\<in>\<^sub>1" 50)
+    -- "Membership in 2/3 set"
+    and member_2 :: "'n \<Rightarrow> 'q2 \<Rightarrow> bool" (infix "\<in>\<^sub>2" 50)
+    -- "Membership in 1/3 set"
+  assumes "\<And> q1 q2 . \<exists> q3 . \<forall> n . n \<in>\<^sub>2 q3 \<longrightarrow> n \<in>\<^sub>1 q1 \<and> n \<in>\<^sub>1 q2"  
+    -- "This is the only property of types @{typ 'q1} and @{typ 'q2} that we need: 
+2/3 quorums have 1/3 intersection"
+
+record ('n,'h)state =
+  -- "@{typ 'n} is the type of validators (nodes), @{typ 'h} hashes, and views are @{typ nat}"
   commit_msg :: "'n \<Rightarrow> 'h \<Rightarrow> nat \<Rightarrow> bool"
-  prepare_msg :: "'n \<Rightarrow> 'h \<Rightarrow> nat \<Rightarrow> 'v \<Rightarrow> bool"
+  prepare_msg :: "'n \<Rightarrow> 'h \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> bool"
 
 locale casper = byz_quorums +
+  -- "Here we make assumptions about hashes. In reality any message containing a hash not satisfying those
+should be dropped."
 fixes 
   hash_parent :: "'h \<Rightarrow> 'h \<Rightarrow> bool" (infix "\<leftarrow>" 50)
 assumes
-  -- "a hash has at most one predecessor which is not itself"
+  -- "a hash has at most one parent which is not itself"
   "\<And> h1 h2 . h1 \<leftarrow> h2 \<Longrightarrow> h1 \<noteq> h2"
   and "\<And> h1 h2 h3 . \<lbrakk>h2 \<leftarrow> h1; h3 \<leftarrow> h1\<rbrakk> \<Longrightarrow> h2 = h3"
 begin
+
+lemmas casper_assms_def = casper_def casper_axioms_def byz_quorums_def
 
 inductive hash_ancestor (infix "\<leftarrow>\<^sup>*" 50) where 
   "h1 \<leftarrow> h2 \<Longrightarrow> h1 \<leftarrow>\<^sup>* h2"
 | "\<lbrakk>h1 \<leftarrow> h2; h2 \<leftarrow>\<^sup>* h3\<rbrakk> \<Longrightarrow> h1 \<leftarrow>\<^sup>* h3"
 declare hash_ancestor.intros[simp,intro]
-lemma hash_ancestor_intro': assumes "h1 \<leftarrow>\<^sup>* h2" and "h2 \<leftarrow> h3" shows "h1 \<leftarrow>\<^sup>* h3" using assms
-  by (induct h1 h2 rule:hash_ancestor.induct) auto
+lemma hash_ancestor_intro': assumes "h1 \<leftarrow>\<^sup>* h2" and "h2 \<leftarrow> h3" shows "h1 \<leftarrow>\<^sup>* h3" 
+  using assms by (induct h1 h2 rule:hash_ancestor.induct) auto
 
 inductive nth_ancestor :: "nat \<Rightarrow> 'h \<Rightarrow> 'h \<Rightarrow> bool" where 
   "nth_ancestor 0 h1 h1"
 | "\<lbrakk>nth_ancestor n h1 h2; h2 \<leftarrow> h3\<rbrakk> \<Longrightarrow> nth_ancestor (n+1) h1 h3"
 declare nth_ancestor.intros[simp,intro]
-
 inductive_cases nth_ancestor_succ:"nth_ancestor (n+1) h1 h3"
 inductive_cases zeroth_ancestor:"nth_ancestor 0 h1 h3"
-
 lemma parent_ancestor:"h1 \<leftarrow> h2 = nth_ancestor 1 h1 h2"
   by (metis One_nat_def add.right_neutral add_Suc_right add_diff_cancel_left' diff_Suc_Suc nat.simps(3) nth_ancestor.simps)
 
-lemma "\<lbrakk>nth_ancestor (n+1) h1 h2; h3 \<leftarrow> h2\<rbrakk> \<Longrightarrow> nth_ancestor n h1 h3"
-  by (metis casper.axioms(2) casper_axioms casper_axioms_def nth_ancestor_succ)
-  
-lemmas casper_assms_def = casper_def casper_axioms_def byz_quorums_def
-
-text {* All messages in epoch @{term 0} are ignored;  @{term 0} is used as a special value.
-  This appears in the following definitions. *}
-
+text {* All messages in epoch @{term "0::nat"} are ignored;  @{term "0::nat"} is used as a special value (was @{term "-1::int"} in the original model). *}
 definition prepared' where
   "prepared' s q h v1 v2 \<equiv> v1 \<noteq> 0 \<and> (\<forall> n . n \<in>\<^sub>1 q \<longrightarrow> prepare_msg s n h v1 v2)"
 definition prepared where
@@ -63,11 +67,6 @@ definition slashed_2 where
   "slashed_2 s n \<equiv>
   \<exists> h v1 v2 . prepare_msg s n h v1 v2 \<and> v2 \<noteq> 0 \<and>
     (\<forall> v3 q h2 . v3 < v2 \<longrightarrow> \<not>(nth_ancestor (v1 - v2) h2 h \<and> prepared s q h2 v2 v3))"
-definition slashed_2' where
-  -- "this is not what Vitalik wrote, but works too."
-  "slashed_2' s n \<equiv>
-  \<exists> h v1 v2 . prepare_msg s n h v1 v2 \<and> v2 \<noteq> 0 \<and>
-    (\<forall> v3 q h2 . v3 < v2 \<longrightarrow> \<not>(h2 \<leftarrow> h \<and> prepared s q h2 v2 v3))"
 definition slashed_3 where 
   "slashed_3 s n \<equiv>
   \<exists> h1 h2 v1 v2 v3 . v1 < v2 \<and> v3 < v1 \<and> 
@@ -85,40 +84,30 @@ lemmas casper_defs = slashed_defs prepared_def fork_def committed_def casper_ass
 
 lemma l1: assumes "prepared s q1 h1 v1 v2" and "committed s q2 h2 v3" and "v1 > v3" and "\<not>one_third_slashed s"
   shows "v1 > v2 \<and> v2 \<ge> v3"
-  nitpick[verbose, card 'b=1, card 'c=1, card 'a=1, card nat=5, card 'h=4, card 'd=1,
-      card "('a, 'h, nat, 'd) state_scheme" = 1, dont_box, timeout=300, eval="fork s"]
-  using assms casper_axioms linorder_axioms[where ?'a=nat]
-  unfolding casper_defs order_defs
+(*<*)  nitpick[verbose, card 'b=1, card 'c=1, card 'a=1, card nat=5, card 'h=4, card 'd=1,
+      card "('a, 'h, 'd) state_scheme" = 1, dont_box, timeout=300, eval="fork s"] (*>*)
+  using assms casper_axioms linorder_axioms[where ?'a=nat] unfolding casper_defs order_defs
   by smt
 
 lemma l2: assumes "nth_ancestor n h1 h2" and "nth_ancestor m h2 h3" 
   shows "nth_ancestor (n+m) h1 h3" 
   using assms 
 proof (induct m arbitrary: h1 h2 h3)
-  case 0
-  then show ?case using nth_ancestor.cases by auto
+  case 0 then show ?case using nth_ancestor.cases by auto
 next
   case (Suc m)
-  then show ?case
-    by (metis Suc_eq_plus1 add_Suc_right nth_ancestor.simps nth_ancestor_succ)
+  then show ?case by (metis Suc_eq_plus1 add_Suc_right nth_ancestor.simps nth_ancestor_succ)
 qed
 
 lemma l3: assumes "prepared s q1 h1 v1 v2" and "committed s q2 h2 v3" and "v1 > v3" and "\<not>one_third_slashed s"
   shows "nth_ancestor (v1 - v3) h2 h1"
-  nitpick[verbose, card 'b=1, card 'c=1, card 'a=1, card nat=5, card 'h=4, card 'd=1,
-      card "('a, 'h, nat, 'd) state_scheme" = 1, dont_box, timeout=300, eval="fork s"]
 proof -
   show ?thesis using assms
   proof (induct "v1 - v3" arbitrary: v1 v2 v3 q1 q2 h1 h2 rule:less_induct)
-    case less
-    then show ?case 
+    case less then show ?case 
     proof (cases "v1-v3=0")
-      case True
-      then show ?thesis
-        using less.prems(3) by linarith 
-    next
-      case False
-      then show ?thesis
+      case True then show ?thesis using less.prems(3) by linarith  next
+      case False then show ?thesis
       proof (cases "v2=v3")
         case True
         then show ?thesis using less(2-5) casper_axioms linorder_axioms[where ?'a=nat]
@@ -136,12 +125,9 @@ proof -
   qed
 qed
 
-lemma l4:assumes "nth_ancestor n (h1::'h) h2"
-  shows "h1 \<leftarrow>\<^sup>* h2 \<or> h1 = h2"
-  using assms
+lemma l4:assumes "nth_ancestor n (h1::'h) h2" shows "h1 \<leftarrow>\<^sup>* h2 \<or> h1 = h2" using assms
 proof (induct n arbitrary:h1 h2)
-  case 0
-  then show ?case using zeroth_ancestor by auto
+  case 0 then show ?case using zeroth_ancestor by auto
 next
   case (Suc n)
   obtain h3 where 1:"h3 \<leftarrow> h2" and 2:"nth_ancestor n h1 h3" using nth_ancestor_succ Suc.prems by (metis Suc_eq_plus1) 
@@ -158,30 +144,14 @@ proof -
   show ?thesis 
   proof (cases "v1 = v2")
     case True
-    show ?thesis using 1 4 2 5 6 True casper_axioms unfolding byz_quorums_def casper_def
-      one_third_slashed_def casper_axioms committed_def prepared_def slashed_1_def slashed_4_def slashed_def
+    show ?thesis using 1 4 2 5 6 True casper_axioms unfolding byz_quorums_def casper_def  slashed_def
+      one_third_slashed_def casper_axioms committed_def prepared_def slashed_1_def slashed_4_def
       by metis
   next
-    case False
-    thus ?thesis using l3 1 2 4 5 6
-      by (metis less_le not_less)
+    case False thus ?thesis using l3 1 2 4 5 6 by (metis less_le not_less)
   qed
 qed
 
-text "Just two observations:"
-
-lemma assumes "\<not>one_third_slashed s" and "committed s q1 h1 v1" and "committed s q2 h2 v2"
-  and "h1 \<leftarrow>\<^sup>* h2" and "h2 \<leftarrow>\<^sup>* h1" shows False
-  nitpick[verbose, card 'b=1, card 'c=1, card 'a=1, card nat=3, card 'h=2, card 'd=1,
-      card "('a, 'h, nat, 'd) state_scheme" = 1, dont_box, timeout=300, eval="(\<lambda> h v1 v2 . \<exists> q . prepared s q h v1 v2)"]
-  -- "The slashing conditions allow loops in the block chain."
-  oops
-
-lemma assumes "\<not>one_third_slashed s" and "committed s q h v"
-  obtains q0 h0 v0 where "prepared s q0 h0 1 v0"
-  nitpick[verbose, card 'b=1, card 'c=1, card 'a=1, card nat=3, card 'h=2, card 'd=1,
-      card "('a, 'h, nat, 'd) state_scheme" = 1, dont_box, timeout=300, eval="fork s"]
-  -- "The slashing conditions allow a chain not anchored at 0."
-  oops
+end
 
 end
